07da0f96247d708dcc84bcb59fe5fb6401d410d0
金沢森 <shin.ne1365@gmail.com>
2025-05-03 05:48:20 +0900

    feat: GET/POSTエンドポイントを追加

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -18,6 +19,30 @@ func main() {
 
 	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "さようなら！")
+	})
+
+	// GETリクエスト
+	http.HandleFunc("/get", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "GETメソッドのみ許可されています", http.StatusMethodNotAllowed)
+			return
+		}
+		fmt.Fprintf(w, "GETリクエストを受け取りました！")
+	})
+
+	// POSTリクエスト
+	http.HandleFunc("/post", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "POSTメソッドのみ許可されています", http.StatusMethodNotAllowed)
+			return
+		}
+		// POSTデータを読み取る
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "リクエストボディの読み取りに失敗しました", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(w, "POSTリクエストを受け取りました！データ: %s", string(body))
 	})
 
 	// ポート番号を指定してサーバーを起動
